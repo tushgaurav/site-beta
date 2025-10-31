524cc6fc6603a01c15a4d61f1c2539f4b4021753
tushgaurav <tushar.gaurav416@gmail.com>
2025-10-31 20:57:08 +0530

    add dark mode toggle

--- src/components/base/navbar.tsx
+++ src/components/base/navbar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { ModeToggle } from '../theme-toggle'
 
 export default function Navbar() {
   return (
@@ -13,8 +14,11 @@ export default function Navbar() {
           className="h-8 transition-all duration-300 hover:drop-shadow-[0_0_15px_rgba(255,165,0,0.8)] hover:brightness-110 hover:animate-pulse"
         />
       </Link>
-      <div className="flex gap-4 text-sm">
-        <Link href="/">Archive</Link>
+      <div className="flex gap-4 text-sm items-center">
+        <div className="flex gap-2 pr-4">
+          <ModeToggle />
+        </div>
+        <Link href="/archive">Archive</Link>
         <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
       </div>
