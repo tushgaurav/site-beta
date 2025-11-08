4daad2628172b94ce1bffad123cb20b6677aca85
Tushar Gaurav <tushar.gaurav416@gmail.com>
2025-11-08 06:57:53 +0530

    fix: footer layout

--- src/components/base/footer.tsx
+++ src/components/base/footer.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import { Separator } from "../ui/separator";
+
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center p-4 limit-width text-muted-foreground mb-2 text-sm">
-      <p>&copy; {new Date().getFullYear()} Tushar Gaurav. All Rights Reserved.</p>
+    <footer>
+      <Separator className="my-2" />
+      <div className="flex justify-end items-center p-4 limit-width text-muted-foreground mb-2 text-sm">
+        <div className="flex justify-center items-center gap-4">
+          <Link href="/privacy-policy" className="hover:text-primary transition-colors">
+            Privacy
+          </Link>
+          <Link href="/terms-of-service" className="hover:text-primary transition-colors">
+            Terms
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center p-2 limit-width text-muted-foreground text-sm">
+        <p>&copy; {new Date().getFullYear()} Tushar Gaurav. All Rights Reserved.</p>
+      </div>
     </footer>
   )
 }
