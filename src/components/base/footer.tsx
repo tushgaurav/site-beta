63e35846a929a65ed4c2fb8727136f4fbc5880a9
tushgaurav <tushar.gaurav416@gmail.com>
2025-11-10 21:43:43 +0530

    add resume link

--- src/components/base/footer.tsx
+++ src/components/base/footer.tsx
@@ -1,11 +1,16 @@
-import Link from "next/link";
-import { Separator } from "../ui/separator";
+import Link from 'next/link'
+import { Separator } from '../ui/separator'
 
 export default function Footer() {
   return (
     <footer>
       <Separator className="my-2" />
-      <div className="flex justify-end items-center p-4 limit-width text-muted-foreground mb-2 text-sm">
+      <div className="flex justify-between items-center p-4 limit-width text-muted-foreground mb-2 text-sm">
+        <div className="flex justify-center items-center gap-4">
+          <Link href="/resume" className="hover:text-primary transition-colors">
+            Resume
+          </Link>
+        </div>
         <div className="flex justify-center items-center gap-4">
           <Link href="/privacy-policy" className="hover:text-primary transition-colors">
             Privacy
